391d9c2d0c8aa24778ad94e084c622532650a485
Chris Sullivan <cs590919@wne.edu>
2023-05-01 23:53:19 -0400

    ADD: main.py

---
+++ main.py
@@ -0,0 +1,26 @@
+""" 
+Markov Decision Processes - main.py
+Christopher D. Sullivan
+Professor Brian O'Neill
+Due 5/4/23
+
+This script is designed to model the differences of three randomized optimization algorithms
+over three optimization problems each. This is achieved through mlrose, a python package 
+designed for machine learning weight optimization, which contains other 
+general fitness functions used for maximization/minimization.
+
+Imports:
+https://pymdptoolbox.readthedocs.io/en/latest/ - mdptoolbox
+
+"""
+
+
+import mdptoolbox.mdp, mdptoolbox.util, mdptoolbox.example
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
