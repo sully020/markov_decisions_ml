95189940d75791d858d29641c0c129c232fe854c
Chris Sullivan <cs590919@wne.edu>
2023-05-04 14:35:08 -0400

    MODIFY: Update gridworld test functions, add action choice

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ https://pymdptoolbox.readthedocs.io/en/latest/ - mdptoolbox
 
 import mdptoolbox.mdp as mdp, mdptoolbox.util as util, mdptoolbox.example as ex
 import grid_world as gw
+import numpy as np
 
-gw_P = gw.P
-gw_R = gw.R
+gw_chanceP = np.array(gw.chance_P)
+gw_chanceR = np.array(gw.chance_R)
+gw_deterministicP = np.array(gw.deterministic_P)
+gw_deterministicR = np.array(gw.deterministic_R)
 
 def new_forest(num_states, fire_prob):
     forest_model = ex.forest(S = num_states, p = fire_prob)
@@ -24,20 +27,52 @@ def new_forest(num_states, fire_prob):
 
 
 def value_iter_gw():
-    pass
+    is_chosen = False
+    while (is_chosen == False):
+        choice = input("Would you like the agent's actions to be deterministic (D) or non-deterministic (N)? ")
+        if choice == 'D' or 'd':
+            gw_vi = mdp.ValueIteration(gw_chanceP, gw_chanceR, .2)
+            is_chosen = True  
+        elif choice == 'N' or 'n':
+            gw_vi = mdp.ValueIteration(gw_deterministicP, gw_deterministicR, .2)
+            is_chosen = True
+        else:
+            print('Input not recognized. Please select the letter D or the letter N.')
 
-def value_iter_forest():
-    pass
+    gw_vi.run()
+    print(gw_vi.policy)
 
 def policy_iter_gw():
-    pass
+    is_chosen = False
+    while (is_chosen == False):
+        choice = input("Would you like the agent's actions to be deterministic (D) or non-deterministic (N)? ")
+        if choice == 'D' or 'd':
+            gw_vi = mdp.PolicyIteration(gw_chanceP, gw_chanceR, .2)
+            is_chosen = True  
+        elif choice == 'N' or 'n':
+            gw_vi = mdp.PolicyIteration(gw_deterministicP, gw_deterministicR, .2)
+            is_chosen = True
+        else:
+            print('Input not recognized. Please select the letter D or the letter N.')
+            
+    gw_vi.run()
+    print(gw_vi.policy)
+
+def forest_input():
+    num_states = input("How many years until the forest reaches its' peak? (Number of states): ")
+    fire_prob = input("Enter an integer for probability of fire p: .")
+
+def value_iter_forest():
+    states, prob = new_forest()
 
 def value_iter_forest():
     pass
 
 def main():
-    pass
+    value_iter_gw()
+    policy_iter_gw()
 
 if __name__ == "__main__":
     main()
 
+# Professor O'Neill, you are the G.O.A.T. Best of luck in your coming endeavors!
